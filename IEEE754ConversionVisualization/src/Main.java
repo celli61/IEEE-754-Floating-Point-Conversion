e7431d2dee4535723ac3e7c30073fb2478b9483d
celli61 <163802499+celli61@users.noreply.github.com>
2025-03-20 13:02:09 -0500

    Completed basic functionality with some floating point precision errors still present

--- IEEE754ConversionVisualization/src/Main.java
+++ IEEE754ConversionVisualization/src/Main.java
@@ -34,8 +34,10 @@ public class Main {
         double decimalNumConvert = (double) ((Integer.parseInt(decimalNum)) * Math.pow(10, -(decimalNum.length())));
         System.out.println("Decimal portion of input: " + decimalNumConvert);
 
+
         //convert from decimal form to binary string with a for loop and a string builder
         StringBuilder decimalNumBinaryBuilder = new StringBuilder();
+
 
         //use a for loop to loop a maximum of 23 times to generate the binary string for the decimal input
         for(int i = 0; i < 23; i++) {
@@ -47,12 +49,60 @@ public class Main {
             decimalNumBinaryBuilder.append(leadingDigit);
             decimalNumConvert = decimalNumConvert % 1;
         }
-
         String decimalNumBinary = decimalNumBinaryBuilder.toString();
-        System.err.println("Decimal portion of input in binary: " + decimalNumBinary);
+        System.out.println("Decimal portion of input in binary: " + decimalNumBinary);
+
 
         //combine whole number binary string and decimal binary string into 1 string 
         String wholeAndDecimalNumBinary = wholeNumBinary+decimalNumBinary;
-        System.err.println("Whole number and decimal portion of input in binary: " + wholeAndDecimalNumBinary);
+        System.out.println("Whole number and decimal portion of input in binary: " + wholeAndDecimalNumBinary);
+
+
+        /*determine exponent size by subtracting the length of the substring from 0 
+        * to the first occurrence of "1" from the length of the entire whole num 
+        * binary string */
+        int exponentValue = wholeNumBinary.length() - wholeNumBinary.substring(0, wholeNumBinary.indexOf('1') + 1).length();
+        System.out.println("Calculated value of the exponent: " + exponentValue);
+
+
+        /*get final binary string for mantissa by taking the substring of the 
+        * combined whole and decimal binary strings after the first occurrence of 1 to the end*/
+        String mantissaBinary = wholeAndDecimalNumBinary.substring(wholeNumBinary.indexOf('1') + 1);
+        //check length of binary string and increase/decrease to 23
+        if(mantissaBinary.length() < 23) {
+            mantissaBinary = mantissaBinary + "0".repeat(23 - mantissaBinary.length());
+        } else {
+            mantissaBinary = mantissaBinary.substring(0, 23);
+        }
+        System.out.println("Binary string for mantissa: " + mantissaBinary);
+        System.out.println("Binary string for mantissa length: " + mantissaBinary.length());
+
+
+        //create string builder for final binary string
+        StringBuilder IEEE754BinaryBuilder = new StringBuilder();
+
+        //assign sign bit based off stored whole number of input
+        if(wholeNum < 0) {
+            IEEE754BinaryBuilder.append("1");
+        } else {
+            IEEE754BinaryBuilder.append("0");
+        }
+
+        //add a space to final string to show different parts of binary representation
+        IEEE754BinaryBuilder.append(" ");
+
+        //calculate the binary string for the exponent bits and append to final binary string
+        String exponentBinary = Integer.toBinaryString(exponentValue + 127);
+        System.out.println("Binary string for final exponent value: " + exponentBinary);
+        IEEE754BinaryBuilder.append(exponentBinary);
+
+        IEEE754BinaryBuilder.append(" ");
+
+        //finally, append the mantissa
+        IEEE754BinaryBuilder.append(mantissaBinary);
+
+        String IEEE754Binary = IEEE754BinaryBuilder.toString();
+        System.out.println(IEEE754Binary);
+
     }   
 }
