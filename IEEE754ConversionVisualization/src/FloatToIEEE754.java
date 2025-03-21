228943be6e41e023f9f32b780461d5e2aedd5344
celli61 <celli61@lsu.edu>
2025-03-21 16:54:35 -0500

    Finalized a barebones UI with functional input including input validation and correct output

--- IEEE754ConversionVisualization/src/FloatToIEEE754.java
+++ IEEE754ConversionVisualization/src/FloatToIEEE754.java
@@ -1,77 +1,56 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class FloatToIEEE754 {
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
 
-        boolean inputValid = false;
-        String floatPointString = "";
-        while(!inputValid){
-            System.out.println("Enter a float value: "); 
-            //read in as a string
-            floatPointString = scan.nextLine();
-            try {
-                //will throw expection if not a valid float
-                new BigDecimal(floatPointString);
-                System.out.println(floatPointString + " entered. Converting and visualizing...");
-                inputValid = true; 
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Please enter a valid floating point number");
-            }
+    public static String convertFloatToIEEE754(String floatString) {
+        //get whole portion of the float as a BigInteger 
+        BigInteger wholePart = BigInteger.ZERO;
+        if(floatString.contains(".")){
+            wholePart = new BigInteger(floatString.substring(0, floatString.indexOf('.')));
+        } else {
+            wholePart = new BigInteger(floatString);
         }
-        
-        //get whole number portion of the float by indexing string and typecasting (all numbers before "." as an int)
-        int wholePart = Integer.parseInt(floatPointString.substring(0, floatPointString.indexOf('.')));
-        System.out.println("Whole number portion of input: "+ wholePart);
-        //convert the whole number integer to a binary string through Java 
-        String wholeNumBinary = Integer.toBinaryString(Math.abs(wholePart));
-        System.out.println("Whole number portion of input in binary: " + wholeNumBinary);
-
         //get decimal portion of the float as a BigDecimal to prevent floating point precision issues
-        BigDecimal decimalPart = new BigDecimal(floatPointString).remainder(BigDecimal.ONE);
-        System.out.println("Decimal portion of input: " + decimalPart);
+        BigDecimal decimalPart = new BigDecimal(floatString).remainder(BigDecimal.ONE);
 
+        //store whole part binary string from BigInteger toString
+        String wholePartBinary = wholePart.toString(2);
 
-        //convert from decimal form to binary string with a for loop and a string builder
-        StringBuilder decimalNumBinaryBuilder = new StringBuilder();
-
-
-        //use a for loop to loop a maximum of 23 times to generate the binary string for the decimal input
-        for(int i = 0; i < 23; i++) {
-            if (decimalPart.compareTo(BigDecimal.ZERO) == 0) break; 
+        //build decimal part binary string 
+        StringBuilder decimalPartBinaryBuilder = new StringBuilder();
+        //iterate 25 times to allow extra bits for rounding if applicable
+        for(int i = 0; i < 25; i++) {
+            //if the decimal part is 0, break
+            if (decimalPart.equals(BigDecimal.ZERO)) break; 
+            //multiply decimal value by 2
             decimalPart = decimalPart.multiply(BigDecimal.valueOf(2)); 
-            int bit = Math.abs(decimalPart.intValue());
-            decimalNumBinaryBuilder.append(bit);
+            //append the whole part of the product to the string builder
+            decimalPartBinaryBuilder.append(Math.abs(decimalPart.intValue()));
+            //set decimal part value to decimal part of product
             decimalPart = decimalPart.remainder(BigDecimal.ONE);
         }
-        String decimalNumBinary = decimalNumBinaryBuilder.toString();
-        System.out.println("Decimal portion of input in binary: " + decimalNumBinary);
+        //store built decimal part binary string
+        String decimalPartBinary =  decimalPartBinaryBuilder.toString();
+        
+        //append whole and decimal part binary strings together
+        String wholePlusDecimalBinary = wholePartBinary + decimalPartBinary;
 
+        /*determine exponent size by subtracting the length of the substring from 0 to the first occurrence of "1" from the length of 
+        the entire whole num binary string */
+        int exponentValue = wholePartBinary.length() - wholePartBinary.substring(0, wholePartBinary.indexOf('1') + 1).length();
 
-        //combine whole number binary string and decimal binary string into 1 string 
-        String wholeAndDecimalNumBinary = wholeNumBinary+decimalNumBinary;
-        System.out.println("Whole number and decimal portion of input in binary: " + wholeAndDecimalNumBinary);
-
-
-        /*determine exponent size by subtracting the length of the substring from 0 
-        * to the first occurrence of "1" from the length of the entire whole num 
-        * binary string */
-        int exponentValue = wholeNumBinary.length() - wholeNumBinary.substring(0, wholeNumBinary.indexOf('1') + 1).length();
-        System.out.println("Calculated value of the exponent: " + exponentValue);
-
-
-        /*get final binary string for mantissa by taking the substring of the 
-        * combined whole and decimal binary strings after the first occurrence of 1 to the end*/
+        //store mantissa bits in a new string
         String mantissaBinary;
-        if (wholePart != 0) {
-            mantissaBinary = wholeAndDecimalNumBinary.substring(1); // Remove leading 1
+        //if the whole part of the float is not 0
+        if (!wholePart.equals(BigInteger.ZERO)) {
+            //remove leading 1 
+            mantissaBinary = wholePlusDecimalBinary.substring(1); 
         } else {
-            mantissaBinary = decimalNumBinary.substring(decimalNumBinary.indexOf("1") + 1);
+            mantissaBinary = decimalPartBinary.substring(decimalPartBinary.indexOf("1") + 1);
         }
 
-        //normalize length of mantissa to 23 floating-point rounding (down) with or padding with 0s (up)
+        //normalize mantissa bit string length to 23 by rounding or padding with 0s
         if (mantissaBinary.length() > 23) {
             //24th bit is guard bit
             if (mantissaBinary.charAt(23) == '1') {
@@ -81,6 +60,7 @@ public class FloatToIEEE754 {
                 //increment int by 1
                 mantissaInt = mantissaInt.add(BigInteger.ONE);
                 //store int as binary string again with BigInt toString, replacing spaces with 0s
+                System.out.println(mantissaInt.toString(2));
                 mantissaBinary = String.format("%23s", mantissaInt.toString(2)).replace(' ', '0');
             } else {
                 //if 0, simply truncate
@@ -90,14 +70,12 @@ public class FloatToIEEE754 {
             //if less than 23 bits, pad with 0s
             mantissaBinary = mantissaBinary + "0".repeat(23 - mantissaBinary.length());
         }
-        System.out.println("Binary string for mantissa: " + mantissaBinary);
-
 
         //create string builder for final binary string
         StringBuilder IEEE754BinaryBuilder = new StringBuilder();
 
         //assign sign bit based off stored whole number of input
-        if(wholePart < 0) {
+        if(wholePart.compareTo(BigInteger.ZERO) < 0) {
             IEEE754BinaryBuilder.append("1");
         } else {
             IEEE754BinaryBuilder.append("0");
@@ -108,16 +86,13 @@ public class FloatToIEEE754 {
 
         //calculate the binary string for the exponent bits and append to final binary string
         String exponentBinary = Integer.toBinaryString(exponentValue + 127);
-        System.out.println("Binary string for final exponent value: " + exponentBinary);
         IEEE754BinaryBuilder.append(exponentBinary);
 
         IEEE754BinaryBuilder.append(" ");
 
         //finally, append the mantissa
         IEEE754BinaryBuilder.append(mantissaBinary);
-
-        String IEEE754Binary = IEEE754BinaryBuilder.toString();
-        System.out.println(IEEE754Binary);
-
-    }   
+        
+        return IEEE754BinaryBuilder.toString();
+    }
 }
--- IEEE754ConversionVisualization/src/MainWindow.java
+++ IEEE754ConversionVisualization/src/MainWindow.java
@@ -5,15 +5,19 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
 import javax.swing.*;
 
 public class MainWindow {
 
     private JFrame frame;
-    private JPanel panel;
-    private JTextField textField;
-    private JLabel label;
+    private JPanel inputPanel;
+    private JPanel outputPanel;
+    private JTextField inputField;
+    private JTextField outputField;
+    private JLabel inputPrompt;
+    private JLabel outputPrompt;
     private JButton button;
 
     public MainWindow() {
@@ -24,29 +28,42 @@ public class MainWindow {
         frame = new JFrame();
         frame.setTitle("IEEE-754 Floating Point Conversion");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setLayout(new BorderLayout(10, 10));
+        frame.setSize(600, 400);
+        frame.setLayout(new FlowLayout());
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
 
-        panel = new JPanel();
-        label = new JLabel("Enter a floating point number:");
-        textField = new JTextField(20);
+        inputPanel = new JPanel();
+        outputPanel = new JPanel();
+        inputPrompt = new JLabel("Enter a floating point number:");
+        outputPrompt = new JLabel("IEEE-754 binary representation:");
+        inputField = new JTextField(20);
+        outputField = new JTextField(34);
         button = new JButton("Convert");
 
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                try {
+                    String floatString = inputField.getText();
+                    new BigDecimal(floatString);
+                    String IEEE754BinaryString = FloatToIEEE754.convertFloatToIEEE754(floatString);
+                    outputField.setText(IEEE754BinaryString);
+                } catch(NumberFormatException ex) {
+                    outputField.setText("not a valid decimal string");
+                }
             }
         });
 
-        panel.add(label);
-        panel.add(textField);
-        panel.add(button);
+        inputPanel.add(inputPrompt);
+        inputPanel.add(inputField);
+        inputPanel.add(button);
 
-        frame.add(panel, BorderLayout.CENTER);
+        outputPanel.add(outputPrompt);
+        outputPanel.add(outputField);
 
+        frame.add(inputPanel);
+        frame.add(outputPanel);
 
         show();
     }
